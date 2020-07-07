b1f8c1445a313c57544df16309cd4c083db20a13
Xavier Johnson <krinkodot22@hotmail.com>
2020-07-06 21:19:13 -0400

    Serve with waitress

--- matrix_imposter_bot/__main__.py
+++ matrix_imposter_bot/__main__.py
@@ -4,11 +4,14 @@ from . import app
 from . import config
 
 
+dev = False
 debug = False
 skip_prep = False
 
 for arg in sys.argv[1:]:
-    if arg == '-g':
+    if arg == '-d':
+        dev = True
+    elif arg == '-g':
         debug = True
     elif arg == '--skip':
         skip_prep = True
@@ -16,12 +19,20 @@ for arg in sys.argv[1:]:
         print(f'Invalid argument: {arg}')
         sys.exit(-1)
 
+if debug:
+    dev = True
+
 if not skip_prep:
     from .meta import prep
     prep()
     sys.argv.append('--skip')
 
 
-app.run(host=config.cfg_settings['appservice']['host'],
-        port=config.cfg_settings['appservice']['port'],
-        debug=debug)
+host=config.cfg_settings['appservice']['host']
+port=config.cfg_settings['appservice']['port']
+
+if not dev:
+    from waitress import serve
+    res = serve(app, host=host, port=port)
+else:
+    app.run(host=host, port=port, debug=debug)
